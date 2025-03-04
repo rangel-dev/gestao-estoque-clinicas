c3c26595f1a187f6a30238cbfd7dee17a84cebd5
geleco <rangel001@gmail.com>
2025-03-04 17:40:03 -0300

    fix: pastas e componentes

---
+++ out/production/projetos/frontend/src/pages/Dashboard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Dashboard = () => {
+    return (
+      <div className="text-4xl font-italic underline">
+        Hello world!
+      </div>
+    )
+  }
+
+  export default Dashboard;
