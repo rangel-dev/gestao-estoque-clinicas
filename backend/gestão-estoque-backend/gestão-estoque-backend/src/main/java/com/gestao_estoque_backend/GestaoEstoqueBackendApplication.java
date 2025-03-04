6b20da4c1c66387ea3a25e4acdacfa0a23abb3bb
lsiin <vitorantonio-7@hotmail.com>
2025-03-04 12:33:03 -0300

    Montando ambiente de desenvolvimento para o backend.

---
+++ backend/gestão-estoque-backend/gestão-estoque-backend/src/main/java/com/gestao_estoque_backend/GestaoEstoqueBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gestao_estoque_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoEstoqueBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoEstoqueBackendApplication.class, args);
+	}
+
+}
---
+++ backend/gestão-estoque-backend/gestão-estoque-backend/src/test/java/com/gestao_estoque_backend/GestaoEstoqueBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestao_estoque_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoEstoqueBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
